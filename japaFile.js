5f5c644bdbe9b5678a55bab0a41c1e717e6efc8b
Harminder Virk <virk.officials@gmail.com>
2020-12-03 17:22:17 +0530

    chore: use @adonisjs/require-ts for running ts code

--- japaFile.js
+++ japaFile.js
@@ -1,6 +1,4 @@
-require('ts-node/register')
+require('@adonisjs/require-ts/build/register')
 
 const { configure } = require('japa')
-configure({
-  files: ['test/**/*.spec.ts']
-})
+configure({ files: ['test/**/*.spec.ts'] })
